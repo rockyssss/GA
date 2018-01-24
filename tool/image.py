9845f2c491c6c95c87dfd516c2a73754c94aa716
Minghua <18279114471@163.com>
2018-01-24 11:20:34 +0800

    平移，镜像，缩放

--- tool/image.py
+++ tool/image.py
@@ -1,10 +1,11 @@
+from dao import rectangle_index as rec
 # 镜像
 def image(spacesarray, coordinate):
     for space in spacesarray:
         if (coordinate == 'x'):
             # Y对应数组的索引
-            space[1] = -space[1]
+            space[rec.Y_MIN_INDEX] = -space[rec.Y_MIN_INDEX]
         else:
             # X对应数组的索引
-            space[0] = -space[0]
+            space[rec.X_MIN_INDEX] = -space[rec.X_MIN_INDEX]
     return spacesarray
--- tool/zoom.py
+++ tool/zoom.py
@@ -1,8 +1,9 @@
+from dao import rectangle_index as rec
 # 缩放
 def zoom(spacesarray, ratio):
     for space in spacesarray:
         # length对应数组下标索引
-        space[4] = space[4] * ratio
+        space[rec.L_INDEX] = space[rec.L_INDEX] * ratio
         # width对应数组下标索引
-        space[5] = space[4] * ratio
+        space[rec.W_INDEX] = space[rec.W_INDEX] * ratio
     return spacesarray
