71cd3151e8222ab1e49859d8dc90b1c9abdd5cd0
Minghua <18279114471@163.com>
2018-01-24 15:06:05 +0800

    平移，镜像，缩放

--- tool/image.py
+++ tool/image.py
@@ -1,11 +1,21 @@
 from dao import rectangle_index as rec
-# 镜像
-def image(spacesarray, coordinate):
-    for space in spacesarray:
+# rectangle镜像
+def image(spacesarray, index, coordinate):
+    if (coordinate == 'x'):
+        # Y对应数组的索引
+        spacesarray[index][rec.Y_MIN_INDEX] = -spacesarray[index][rec.Y_MIN_INDEX]
+    else:
+        # X对应数组的索引
+        spacesarray[index][rec.X_MIN_INDEX] = -spacesarray[index][rec.X_MIN_INDEX]
+    return spacesarray
+
+def imagespace(spacesarray, spacesdic, spaceid, coordinate):
+    listrec = spacesdic.get(spaceid)
+    for index in listrec:
         if (coordinate == 'x'):
             # Y对应数组的索引
-            space[rec.Y_MIN_INDEX] = -space[rec.Y_MIN_INDEX]
+            spacesarray[index][rec.Y_MIN_INDEX] = -spacesarray[index][rec.Y_MIN_INDEX]
         else:
             # X对应数组的索引
-            space[rec.X_MIN_INDEX] = -space[rec.X_MIN_INDEX]
+            spacesarray[index][rec.X_MIN_INDEX] = -spacesarray[index][rec.X_MIN_INDEX]
     return spacesarray
--- tool/zoom.py
+++ tool/zoom.py
@@ -1,9 +1,19 @@
 from dao import rectangle_index as rec
-# 缩放
-def zoom(spacesarray, ratio):
-    for space in spacesarray:
-        # length对应数组下标索引
-        space[rec.L_INDEX] = space[rec.L_INDEX] * ratio
-        # width对应数组下标索引
-        space[rec.W_INDEX] = space[rec.W_INDEX] * ratio
+# rectangle缩放
+def zoom(spacesarray, index, ratio):
+    # length对应数组下标索引
+    spacesarray[index][rec.L_INDEX] = spacesarray[index][rec.L_INDEX] * ratio
+    # width对应数组下标索引
+    spacesarray[index][rec.W_INDEX] = spacesarray[index][rec.W_INDEX] * ratio
     return spacesarray
+
+# space缩放
+
+def zoomspace(spacesarray, spacesdic, spaceid, ratio):
+    listrec = spacesdic.get(spaceid)
+    for index in listrec:
+        spacesarray[index][rec.L_INDEX] = spacesarray[index][rec.L_INDEX] * ratio
+        # Y对应的数组下标
+        spacesarray[index][rec.W_INDEX] = spacesarray[index][rec.W_INDEX] * ratio
+    return spacesarray
+
